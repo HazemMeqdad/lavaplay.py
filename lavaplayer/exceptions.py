7273f70bbde1e79ce2f1f72406e3a5ed601dc1b5
HazemMeqdad <hazemmeqdad@gmail.com>
2022-01-22 00:59:49 +0200

    Add description for exceptions objects

--- lavaplayer/exceptions.py
+++ lavaplayer/exceptions.py
@@ -1,45 +1,94 @@
 
 class NodeError(Exception):
+    """
+    A error for node.
+    
+    Parameters
+    ----------
+    message: :class:`str`
+        the error message
+    guild_id: :class:`int`
+        the guild id
+    """
     def __init__(self, message: str, guild_id: int) -> None:
         self._message = message
         self._guild_id = guild_id
 
     @property
     def message(self):
+        """
+        A error message.
+        """
         return self._message
     
     @property
     def guild_id(self):
+        """
+        A guild id.
+        """
         return self._guild_id
 
 
 class VolumeError(Exception):
+    """
+    A error for volume range if not in 0 to 1000.
+    
+    Parameters
+    ----------
+    message: :class:`str`
+        the error message
+    guild_id: :class:`int`
+        the guild id
+    """
     def __init__(self, message: str, guild_id) -> None:
         self._message = message
         self._guild_id = guild_id
 
     @property
     def message(self):
+        """
+        A error message.
+        """
         return self._message
 
     @property
     def guild_id(self):
+        """
+        A guild id.
+        """
         return self._guild_id
 
 
 class NotConnectedError(Exception):
+    """
+    A error for not connected.
+    
+    Parameters
+    ----------
+    message: :class:`str`
+        the error message
+    """
     def __init__(self, message: str) -> None:
         self._message = message
 
     @property
     def message(self):
+        """
+        A error message.
+        """
         return self._message
 
 
 class ConnectedError(Exception):
+    """
+    A error for connected.
+    """
     def __init__(self, message: str) -> None:
         self._message = message
 
     @property
     def message(self):
+        """
+        A error message.
+        """
         return self._message
