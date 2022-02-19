18f1383c075bef24da7b10a64ae70ecbf83723ce
HazemMeqdad <hazemmeqdad@gmail.com>
2022-02-19 22:18:49 +0200

    Update version because the release in pypi hava a problem

--- lavaplayer/__init__.py
+++ lavaplayer/__init__.py
@@ -8,7 +8,7 @@ Lavalink connecter
 __title__ = "lavaplayer"
 __author__ = "HazemMeqdad"
 __license__ = "MIT"
-__version__ = "1.0.6a"
+__version__ = "1.0.7a"
 
 from .client import LavalinkClient
 from .objects import *
