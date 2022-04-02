5bdc1aacacb6395a96a87a22354bcfc6486a1744
TaIFeel <94287800+TaIFeel@users.noreply.github.com>
2022-04-02 19:45:31 +0300

    Exit channels when the bot is turned off.
    If the bot does not exit the channel and stay in it. Then in the next start it will not play music, because the session is unknown. And you have to disable the bot manually

--- examples/dpy_base/bot.py
+++ examples/dpy_base/bot.py
@@ -15,6 +15,13 @@ lavalink = lavaplayer.LavalinkClient(
 
 
 @bot.event
+async def close():
+	for guild in bot.guilds:
+		await guild.change_voice_state(channel=None)
+
+	print(Fore.GREEN + 'Shut down...')
+
+@bot.event
 async def on_ready():
     print("Bot is ready.")
 
