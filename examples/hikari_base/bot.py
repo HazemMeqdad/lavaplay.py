0213e7b7a84d96e28874d7e19a65ac2ce20d370c
HazemMeqdad <hazemmeqdad@gmail.com>
2023-05-30 18:53:17 +0300

    Update a hikari version to ve stable no a new version

--- examples/hikari_base/bot.py
+++ examples/hikari_base/bot.py
@@ -1,4 +1,3 @@
-import asyncio
 import lavaplay
 import hikari
 import logging
@@ -6,20 +5,57 @@ import os
 
 
 PREFIX = ","  # Replace with your prefix
-TOKEN = "..."  # Replace with your token
+TOKEN = os.environ["TOKEN"]  # Replace with your token
 
-bot = hikari.GatewayBot(TOKEN)
-lavalink = lavaplay.LavalinkClient(
-    host="localhost",  # Lavalink host
-    port=2333,  # Lavalink port
-    password="youshallnotpass",  # Lavalink password
-)
+class Bot(hikari.GatewayBot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._lavalink: lavaplay.Node = None
 
-@bot.listen(hikari.StartedEvent)
-async def on_start(event: hikari.StartedEvent):
-    lavalink.set_user_id(bot.get_me().id)
-    lavalink.set_event_loop(asyncio.get_event_loop())
-    lavalink.connect()
+    @property
+    def lavalink(self) -> lavaplay.Node:
+        return self._lavalink
+
+    async def on_start(self, event: hikari.StartedEvent):
+        lava = lavaplay.Lavalink()
+        self._lavalink = lava.create_node(
+            host="localhost",  # Lavalink host
+            port=2333,  # Lavalink port
+            password="youshallnotpass",  # Lavalink password
+            user_id=self.get_me().id,  # Your bot user id
+        )
+        self.lavalink.connect()
+        self.lavalink.event_manager.add_listener(lavaplay.ReadyEvent, self.on_lava_ready)
+        self.lavalink.event_manager.add_listener(lavaplay.TrackStartEvent, self.track_start_event)
+        self.lavalink.event_manager.add_listener(lavaplay.TrackEndEvent, self.track_end_event)
+        self.lavalink.event_manager.add_listener(lavaplay.WebSocketClosedEvent, self.web_socket_closed_event)
+
+    def run(self):
+        self.event_manager.subscribe(hikari.StartedEvent, self.on_start)
+        super().run()
+    # -------------------------------- #
+    # Lavalink events
+    async def on_lava_ready(self, event: lavaplay.ReadyEvent):
+        logging.info(event)
+        logging.info("Lavalink is connected successfully!")
+
+    # track start event
+    async def track_start_event(event: lavaplay.TrackStartEvent):
+        logging.info(f"start track: {event.track.title}")
+
+
+    # track end event
+    async def track_end_event(event: lavaplay.TrackEndEvent):
+        logging.info(f"track end: {event.track.title}")
+
+
+    # web socket closed event
+    async def web_socket_closed_event(event: lavaplay.WebSocketClosedEvent):
+        logging.error(f"error with websocket {event.reason}")
+    # -------------------------------- #
+
+
+bot = Bot(TOKEN, intents=hikari.Intents.ALL)
 
 # https://github.com/vicky5124/lavasnek_rs/blob/master/examples/pure_hikari_basic_queue/bot.py#L16
 def is_command(cmd_name: str, content: str) -> bool:
@@ -29,11 +65,13 @@ def is_command(cmd_name: str, content: str) -> bool:
 # On voice state update the bot will update the lavalink node
 @bot.listen(hikari.VoiceStateUpdateEvent)
 async def voice_state_update(event: hikari.VoiceStateUpdateEvent):
-    await lavalink.raw_voice_state_update(event.guild_id, event.state.user_id, event.state.session_id, event.state.channel_id)
+    player = bot.lavalink.get_player(event.guild_id)
+    await player.raw_voice_state_update(event.state.user_id, event.state.session_id, event.state.channel_id)
 
 @bot.listen(hikari.VoiceServerUpdateEvent)
 async def voice_server_update(event: hikari.VoiceServerUpdateEvent):
-    await lavalink.raw_voice_server_update(event.guild_id, event.endpoint, event.token)
+    player = bot.lavalink.get_player(event.guild_id)
+    await player.raw_voice_server_update(event.endpoint, event.token)
 
 # On message create the bot will play the audio and other commands
 @bot.listen(hikari.GuildMessageCreateEvent)
@@ -41,25 +79,27 @@ async def message_create(event: hikari.GuildMessageCreateEvent):
 
     if not event.message.content:
         return
-
+    lavalink = bot.lavalink
+    player = lavalink.get_player(event.guild_id)
     if is_command("join", event.content):
-        states = bot.cache.get_voice_states_view_for_guild(event.guild_id)
-        voice_state = [state async for state in states.iterator().filter(lambda i: i.user_id == event.author_id)]
-        channel_id = voice_state[0].channel_id
+        state = bot.cache.get_voice_state(event.guild_id, event.author_id)
+        if not state or not state.channel_id:
+            await event.message.respond("You are not in a voice channel")
+            return
+        lavalink.create_player(event.guild_id)
+        channel_id = state.channel_id
         await bot.update_voice_state(event.guild_id, channel_id, self_deaf=True)
-        await lavalink.wait_for_connection(event.guild_id)
         await event.message.respond(f"Joined <#{channel_id}>")
 
     elif is_command("leave", event.content):
         await bot.update_voice_state(event.guild_id, None)
+        await player.destroy()
         await event.message.respond("The bot has left the voice channel")
 
     elif is_command("position", event.content):
-        node = await lavalink.get_guild_node(event.guild_id)
-        await event.message.respond(f"Position: {node.queue[0].position}s")
+        await event.message.respond(f"Position: {player.queue[0].position}s")
 
     elif is_command("play", event.content):
-        print(event.content.replace(f"{PREFIX}play ", ""))
         result = await lavalink.auto_search_tracks(event.content.replace(f"{PREFIX}play ", ""))
 
         if not result:
@@ -69,58 +109,56 @@ async def message_create(event: hikari.GuildMessageCreateEvent):
             await event.message.respond(f"Could not load track: {result.message}")
             return
         elif isinstance(result, lavaplay.PlayList):
-            await lavalink.add_to_queue(event.guild_id, result.tracks, event.author_id)
+            await player.add_to_queue(result.tracks, event.author_id)
             await event.message.respond(f"Added playlist {result.name}")
             return
-        await lavalink.play(event.guild_id, result[0], event.author_id)
+        await player.play(result[0], event.author_id)
         await event.message.respond(f"Now playing {result[0].title}")
 
     elif is_command("pause", event.content):
-        await lavalink.pause(event.guild_id, True)
+        await player.pause(True)
         await event.message.respond("The player has paused")
 
     elif is_command("resume", event.content):
-        await lavalink.pause(event.guild_id, False)
+        await player.pause(False)
         await event.message.respond("The player has resumed")
     
     elif is_command("stop", event.content):
-        await lavalink.stop(event.guild_id)
+        await player.stop()
         await event.message.respond("The player has stopped")
 
     elif is_command("skip", event.content):
-        await lavalink.skip(event.guild_id)
+        await player.skip()
         await event.message.respond("The player has skipped the current song")
 
     elif is_command("queue", event.content):
-        queue = await lavalink.queue(event.guild_id)
-        await event.message.respond("\n".join([f"{i + 1}. {track.title}" for i, track in enumerate(queue)]))
+        await event.message.respond("\n".join([f"{i + 1}. {track.title}" for i, track in enumerate(player.queue)]))
 
     elif is_command("volume", event.content):
         volume = int(event.content.removeprefix(f"{PREFIX}volume "))
-        await lavalink.volume(event.guild_id, volume)
+        await player.volume(volume)
         await event.message.respond(f"The player has set the volume to {volume}")
 
     elif is_command("np", event.content):
-        queue = await lavalink.queue(event.guild_id)
-        if not queue:
+        if not player.queue:
             await event.message.respond("Nothing playing.")
             return
-        await event.message.respond(f"Now playing {queue[0].title}")
+        await event.message.respond(f"Now playing {player.queue[0].title}")
 
     elif is_command("shuffle", event.content):
-        await lavalink.shuffle(event.guild_id)
+        player.shuffle()
         await event.message.respond("The bot has shuffled the queue")
     
     elif is_command("repeat", event.content):
-        await lavalink.repeat(event.guild_id, True)
+        player.repeat(True)
         await event.message.respond("The bot has set the queue to repeat")
     
     elif is_command("norepeat", event.content):
-        await lavalink.repeat(event.guild_id, False)
+        player.repeat(False)
         await event.message.respond("The player has set the queue to not repeat")
 
     elif is_command("list_repeat", event.content):
-        await lavalink.queue_repeat(event.guild_id, True)
+        player.queue_repeat(True)
         await event.message.respond("The bot has set the queue to repeat")
 
     elif is_command("search", event.content):
@@ -146,17 +184,17 @@ async def message_create(event: hikari.GuildMessageCreateEvent):
             await event.message.respond("Please specify a valid position.")
             return
 
-        await lavalink.seek(event.guild_id, position)
+        await player.seek(position)
         await event.message.respond("The track has been seeked.")
 
     elif is_command("destroy", event.content):
-        await lavalink.destroy(event.guild_id)
+        await player.destroy()
         await event.message.respond("The player has been destroyed.")
 
     elif is_command("filter", event.content):
         filters = lavaplay.Filters()
         filters.low_pass(50)
-        await lavalink.filters(event.guild_id, filters)
+        await player.filters(filters)
 
     elif is_command("help", event.content):
         commands = [
@@ -167,26 +205,6 @@ async def message_create(event: hikari.GuildMessageCreateEvent):
             "filter", "list_repeat"
         ]
         await event.message.respond(", ".join([f"`{PREFIX}{command}`" for command in commands]))
-
-# -------------------------------- #
-# track start event
-@lavalink.listen(lavaplay.TrackStartEvent)
-async def track_start_event(event: lavaplay.TrackStartEvent):
-    logging.info(f"start track: {event.track.title}")
-
-
-# track end event
-@lavalink.listen(lavaplay.TrackEndEvent)
-async def track_end_event(event: lavaplay.TrackEndEvent):
-    logging.info(f"track end: {event.track.title}")
-
-
-# web socket closed event
-@lavalink.listen(lavaplay.WebSocketClosedEvent)
-async def web_socket_closed_event(event: lavaplay.WebSocketClosedEvent):
-    logging.error(f"error with websocket {event.reason}")
-# -------------------------------- #
-
 
 if __name__ == "__main__":
     if os.name != "nt":
