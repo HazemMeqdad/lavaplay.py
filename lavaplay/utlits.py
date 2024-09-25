d47ee6ce2e9d129afed8b2dee4aeafd8cb5730d9
lavecat <153299718+lavecat@users.noreply.github.com>
2024-09-25 13:28:09 +0200

    Update utlits.py

--- lavaplay/utlits.py
+++ lavaplay/utlits.py
@@ -30,7 +30,7 @@ def prossing_tracks(tracks: list) -> t.List[Track]:
         info = track["info"]
         list_tracks.append(
             Track(
-                encoded=track["encoded"],  # Use 'encoded' instead of 'track'
+                encoded=track["encoded"],
                 identifier=info["identifier"],
                 is_seekable=info["isSeekable"],
                 author=info["author"],
