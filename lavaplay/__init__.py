6c4e464a1f9a4ca839bf12733f61baa211fa8b60
Hazem Meqdad <hazemmeqdad@outlook.com>
2025-04-05 13:34:27 +0300

    feat: new version

--- lavaplay/__init__.py
+++ lavaplay/__init__.py
@@ -8,7 +8,7 @@ Lavalink connecter
 __title__ = "lavaplay.py"
 __author__ = "HazemMeqdad"
 __license__ = "MIT"
-__version__ = "1.0.14a"
+__version__ = "1.0.15a"
 
 from .client import Lavalink
 from .objects import *
